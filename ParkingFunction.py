41f47e157cb5dae01b56b6998980f0252fc4851b
Jeffrey Saito <youlostyourlife@gmail.com>
2026-07-14 16:42:07 -0400

    Update ParkingFunction.py

--- ParkingFunction.py
+++ ParkingFunction.py
@@ -22,6 +22,23 @@ class ParkingFunction(object):
                 occ.append(k)
         return occ
 
+    def d_v(self, i):
+        a = self.get_area_vector()
+        occ = self.get_occupants_vector()
+
+        count = 0
+
+        for j in range(i + 1, len(a)):
+            # primary dinv contribution
+            if a[i] == a[j] and occ[i] < occ[j]:
+                count += 1
+
+            # secondary dinv contribution
+            if a[i] == a[j] + 1 and occ[i] > occ[j]:
+                count += 1
+
+        return count
+
     def dinv(self):
         occ = self.get_occupants_vector()
         a = self.get_area_vector()
@@ -49,8 +66,8 @@ class ParkingFunction(object):
                 rises.add(i)
         return rises
 
-    # Row i has a moveable valley iff you can move the car in row i one spot to the left and still have a valid PF
-    def moveable_valleys(self):
+    # Row i has a movable valley iff you can move the car in row i one spot to the left and still have a valid PF
+    def movable_valleys(self):
         a = self.get_area_vector()
         occ = self.get_occupants_vector()
         n = self.n
